bb8979cbaef8c98eabe5d777a1a4d1be7f3ff63c
김세윤 <sykimy@naver.com>
2016-12-24 00:34:27 +0900

    Initial Commit

---
+++ Bugs4Mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bugs4Mac
+//
+//  Created by 김세윤 on 2016. 12. 24..
+//  Copyright © 2016년 sykimy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bugs4MacTests/Bugs4MacTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bugs4MacTests.swift
+//  Bugs4MacTests
+//
+//  Created by 김세윤 on 2016. 12. 24..
+//  Copyright © 2016년 sykimy. All rights reserved.
+//
+
+import XCTest
+@testable import Bugs4Mac
+
+class Bugs4MacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
